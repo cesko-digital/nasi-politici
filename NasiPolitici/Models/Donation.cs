f9cf2c253a4958d0034dfb1c2daafb5fd650f6ad
siroky.honza <siroky.honza@gmail.com>
2019-11-28 11:18:36 +0100

    English schema

---
+++ NasiPolitici/Models/Donation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HlidacStatu.NasiPolitici.Models
+{
+    public sealed class Donation
+    {
+        public string Party { get; set; }
+        public decimal Value { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Origin { get; set; }
+    }
+}
---
+++ NasiPolitici/Models/PersonSearchResult.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace HlidacStatu.NasiPolitici.Models
+{
+    public class PersonSearchResult
+    {
+        public List<PersonSummary> Persons { get; set; }
+    }
+}
