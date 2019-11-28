0ce50e77b92524994bb9f941bb2aa8f3593b2413
unknown <standa.kurik@gmail.com>
2019-11-28 11:34:25 +0100

    WIP

---
+++ NasiPolitici/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using HlidacStatu.NasiPolitici.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace HlidacStatu.NasiPolitici.Controllers
+{
+    public class PersonController : Controller
+    {
+        public List<PersonSearchResult> Search(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Detail(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
