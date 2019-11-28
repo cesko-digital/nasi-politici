33dfa758575747f3c01863e31f91ef931aba23da
unknown <standa.kurik@gmail.com>
2019-11-28 10:52:58 +0100

    Add a Donation model

---
+++ NasiPolitici/Models/Donation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HlidacStatu.NasiPolitici.Models
+{
+    public sealed class Donation
+    {
+        public string Party { get; set; }
+        public decimal Value { get; set; }
+        public DateTime Date { get; set; }
+        public string Origin { get; set; }
+    }
+}
