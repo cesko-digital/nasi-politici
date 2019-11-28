ab0cb1d83746305476abaa46355207eddf149bff
unknown <standa.kurik@gmail.com>
2019-11-28 11:55:20 +0100

    WIP

--- NasiPolitici/Models/Donation.cs
+++ NasiPolitici/Models/Donation.cs
@@ -1,26 +1,12 @@
 ﻿using System;
-<<<<<<< HEAD
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-=======
->>>>>>> origin
 
 namespace HlidacStatu.NasiPolitici.Models
 {
     public sealed class Donation
     {
         public string Party { get; set; }
-<<<<<<< HEAD
-        public decimal Value { get; set; }
-        public int Year { get; set; }
-        public string Origin { get; set; }
-    }
-}
-=======
         public string Origin { get; set; }
         public decimal Value { get; set; }
         public DateTime Date { get; set; }
     }
 }
->>>>>>> origin
