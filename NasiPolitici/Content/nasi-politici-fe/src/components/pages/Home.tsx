e234da560a573c6851180f9a326c92ef55c85961
Ludovit Scholtz <ludovit.scholtz@wood.cz>
2019-11-28 15:10:14 +0100

    add pages and shared

--- NasiPolitici/Content/nasi-politici-fe/src/components/pages/Home.tsx
+++ NasiPolitici/Content/nasi-politici-fe/src/components/pages/Home.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Header from '../shared/header';
 
 export const Home = () => {
     return (
-        <div>I am home</div>
+        <div>
+            <Header/>
+            I am home
+        </div>
     );
 }
