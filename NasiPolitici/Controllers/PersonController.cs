2c9755b15e7f4d2a66131abf8d080d2b1dc7675a
unknown <standa.kurik@gmail.com>
2019-11-28 11:54:25 +0100

    WIP

--- NasiPolitici/Controllers/PersonController.cs
+++ NasiPolitici/Controllers/PersonController.cs
@@ -1,15 +1,47 @@
 ﻿using HlidacStatu.NasiPolitici.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
 using System.Collections.Generic;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace HlidacStatu.NasiPolitici.Controllers
 {
+    public class ExternalSearchResult
+    {
+        public List<object> Items { get; }
+    }
+    public class ExternalDetail
+    {
+
+    }
+
+    public interface IDataProvider
+    {
+        ExternalSearchResult Search(string query);
+        ExternalDetail GetDetail(string id);
+    }
+
     public class PersonController : Controller
     {
-        public List<PersonSearchResult> Search(string query)
+        private readonly IDataProvider dataProvider;
+        private IMemoryCache cache;
+
+        public PersonController(IMemoryCache cache)
         {
-            throw new NotImplementedException();
+            this.cache = cache;
+        }
+
+        public PersonSearchResult Search(string query)
+        {
+            var searchResult = dataProvider.Search(query);
+            return new PersonSearchResult
+            {
+                Persons = searchResult.Items.Select(p => new PersonSummary
+                {
+                    FirstName = "Karel"
+                }).ToList()
+            };
         }
 
         public Person Detail(string id)
