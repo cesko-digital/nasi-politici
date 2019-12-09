4ff89b36d71625348650fc2ecc42e0c07808ba77
Nikolay Khodov <i@valt.me>
2019-12-09 16:20:56 +0100

    Finish styling the app bar on desktops

--- NasiPolitici/Content/nasi-politici-fe/src/components/ui/AppBar/AppBar.tsx
+++ NasiPolitici/Content/nasi-politici-fe/src/components/ui/AppBar/AppBar.tsx
@@ -1,53 +1,90 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { InputBase, IconButton, AppBar as MuiAppBar, Toolbar } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
-import styled from 'styled-components';
-import { useDesktop } from '../../../hooks';
-import {SearchInput} from "../SearchInput";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Box } from "@material-ui/core";
+import styled from "styled-components";
+import { useDesktop } from "../../../hooks";
+import { SearchInput } from "../SearchInput";
+import Grid from "@material-ui/core/Grid";
 
 const texts = {
-    about: 'O projektu',
-    media: 'Pro médiá'
+  about: "O projektu",
+  media: "Pro médiá"
 };
 
-const LeftAppBarSide = styled.div`
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    & > * {
-        margin-right: ${props => props.theme.spacing(2)}px;
-    }
+const Toolbar = styled.div`
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  border-bottom: 1px solid gray;
+  box-sizing: border-box;
 
-    flex-grow: 1;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`;
+
+const AppBarLink = styled(Link)`
+  & {
+    text-decoration: none;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    font-size: 17px;
+    color: black;
+    cursor: pointer;
+  }
+`;
+
+const ToolbarWrapper = styled(Box)`
+  display: flex;
+  flex-direction: row;
+    align-items: center;
+  flex-wrap: wrap;
+`;
+
+const LeftAppBarSide = styled.span`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  & > * {
+    margin-right: ${props => props.theme.spacing(2)}px;
+  }
+
+  flex-grow: 1;
 `;
 
 const AppBar = () => {
-    const isDesktop = useDesktop();
-    console.log(isDesktop);
-        return (
-            <MuiAppBar color="default">
-                <Toolbar>
-                    <LeftAppBarSide>
-                        <Link to="/">
-                            {/* I belive that should not be an image */}
-                            <img
-                                src="/assets/img/np-logo-black.png"
-                                width="300px"
-                                alt="Logo NašiPolitici.cz"
-                            ></img>
-                        </Link>
-                        <Link to="/about">{texts.about}</Link>
-                        <Link to="/media">{texts.media}</Link>
-                    </LeftAppBarSide>
-                    {!isDesktop &&
-                        <>
-                          <SearchInput/>
-                    </>
-                    }
-                </Toolbar>
-            </MuiAppBar>
-        );
+  const isDesktop = useDesktop();
+  return (
+    <Toolbar>
+      <Grid container justify="center" alignItems="center">
+        <Grid item xs={10}>
+          <ToolbarWrapper>
+            <LeftAppBarSide>
+              <Link to="/">
+                {/* I belive that should not be an image */}
+                <img
+                  src="/assets/img/np-logo-black.png"
+                  width="300px"
+                  alt="Logo NašiPolitici.cz"
+                />
+              </Link>
+              <AppBarLink className="header-menu-item" to="/about">
+                {texts.about}
+              </AppBarLink>
+              <AppBarLink className="header-menu-item" to="/media">
+                {texts.media}
+              </AppBarLink>
+            </LeftAppBarSide>
+
+            {isDesktop && (
+              <>
+                <SearchInput />
+              </>
+            )}
+          </ToolbarWrapper>
+        </Grid>
+      </Grid>
+    </Toolbar>
+  );
 };
 
 export default AppBar;
--- NasiPolitici/Content/nasi-politici-fe/src/components/ui/SearchInput/index.tsx
+++ NasiPolitici/Content/nasi-politici-fe/src/components/ui/SearchInput/index.tsx
@@ -3,18 +3,24 @@ import styled from "styled-components";
 import { Box, Input, InputAdornment } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
-export const SearchTextInput = styled(Input)`
+const SearchInputContainer = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  font-size: 17px;
+  height: 40px;
+`;
+
+export const SearchInputText = styled(Input)`
   background: #ffffff;
   border: 1px solid #b3b3b3;
   box-sizing: border-box;
   box-shadow: inset 0px 2px 4px rgba(0, 0, 0, 0.1);
   border-radius: 20px 0px 0px 20px;
-  width: 100%;
-  padding: 10px;
-  font-size: 21px;
+  max-height: 100%;
+  padding-left: 10px;
 `;
 
-SearchTextInput.defaultProps = {
+SearchInputText.defaultProps = {
   disableUnderline: true,
   placeholder: "Kalousek",
   startAdornment: (
@@ -24,25 +30,21 @@ SearchTextInput.defaultProps = {
   )
 };
 
-export const SearchSubmitButton = styled.button`
+export const SearchInputButton = styled.button`
   border-radius: 0px 20px 20px 0px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border: 1px solid black;
+  border: none;
   color: white;
   background-color: #307abe;
   white-space: nowrap;
-
-  font-weight: 600;
   line-height: 100%;
-
-  padding: 0px 18px;
+  max-height: 100%;
 `;
 
 export const SearchInput: React.FunctionComponent = props => {
   return (
-    <Box display="flex" flexDirection="row">
-      <SearchTextInput />
-      <SearchSubmitButton>Hledat&nbsp;politika/čku</SearchSubmitButton>
-    </Box>
+    <SearchInputContainer>
+      <SearchInputText />
+      <SearchInputButton>Hledat&nbsp;politika/čku</SearchInputButton>
+    </SearchInputContainer>
   );
 };
