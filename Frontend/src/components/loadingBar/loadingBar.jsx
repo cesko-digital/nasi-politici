6f7559ada57689486602f07c1783206bd2b2670d
Michael Jenis <michael.jenis@socialbakers.com>
2020-01-11 19:41:17 +0100

    64-bug-padding-margin-oddělovačů-sekcí

--- Frontend/src/components/loadingBar/loadingBar.jsx
+++ Frontend/src/components/loadingBar/loadingBar.jsx
@@ -1,4 +1,16 @@
 import React from 'react'
 import styles from './loadingBar.module.scss'
 
-export default () => <div className={styles.ldsRoller}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+export default () =>
+  <div className={styles.roller}>
+    <div className={styles.wrapper}>
+      <div className={styles.circle}></div>
+      <div className={styles.circle}></div>
+      <div className={styles.circle}></div>
+      <div className={styles.circle}></div>
+      <div className={styles.circle}></div>
+      <div className={styles.circle}></div>
+      <div className={styles.circle}></div>
+      <div className={styles.circle}></div>
+    </div>
+  </div>
