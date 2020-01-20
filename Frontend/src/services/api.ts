8185826fbe1e3936d08673359067c526e9a01339
Jan Pavlovsky <jan.pavlovsky@socialbakers.com>
2020-01-20 19:44:14 +0100

    Use axios instead of fetch API

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,54 +1,28 @@
 import { API } from "./apiTypes"
+import axios from 'axios'
 
 const api: API = {
 	search: async (query) => {
-		const response = await fetch(new Request(`${process.env.REACT_APP_BASE_API_URL}/api/v1/person/search/${query}`, {
-			method: 'GET',
-			mode: 'cors',
-			redirect: 'follow',
-			headers: new Headers({
-					"Accept": "application/json",
-        })
-      }))
-      return response.json()
+		const response = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/api/v1/person/search/${query}`)
+		return response.data
   },
   fetchDetail: async (id) => {
-      const response = await fetch(new Request(`${process.env.REACT_APP_BASE_API_URL}/api/v1/person/detail/${id}`, {
-        method: 'GET',
-        mode: 'cors',
-        redirect: 'follow',
-        headers: new Headers({
-          "Accept": "application/json",
-			})
-		}))
-		return response.json()
+		const response = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/api/v1/person/detail/${id}`)
+		return response.data
 	},
   fetchNews: async (fullName, party, searchQuery) => {
-      const response = await fetch(new Request(`${process.env.REACT_APP_BASE_API_URL}/api/v1/news/monitora`, {
-        method: 'POST',
-        mode: 'cors',
-        redirect: 'follow',
-        headers: new Headers({
-          "Accept": "application/json",
-        }),
-        body: JSON.stringify({"data": {
-          "name": fullName,
-          "party": party,
-          "search_query": searchQuery,
-        }})
-    }))
-    return response.json()
+		const response = await axios.post(`${process.env.REACT_APP_BASE_API_URL}/api/v1/news/monitora`, {
+			"data": {
+				"name": fullName,
+				"party": party,
+				"search_query": searchQuery,
+			}
+		})
+    return response.data
 	},
 	fetchDemagog: async (id) => {
-			const response = await fetch(new Request(`${process.env.REACT_APP_BASE_API_URL}/api/v1/demagog/${id}`, {
-				method: 'GET',
-				mode: 'cors',
-				redirect: 'follow',
-				headers: new Headers({
-					"Accept": "application/json",
-			})
-		}))
-		return response.json()
+		const response = await axios.get(`${process.env.REACT_APP_BASE_API_URL}/api/v1/demagog/${id}`)
+		return response.data
   }
 }
 
