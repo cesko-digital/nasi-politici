cce42478dcf2d3f924bdbf9c24465e903f4c8eba
Mila Votradovec <me@milavotradovec.cz>
2020-02-24 22:20:29 +0100

    fix: get token for monitora backend (#43)
    * fix: get token for monitora backend

--- Monitora-backend/app/data/token.py
+++ Monitora-backend/app/data/token.py
@@ -1,1 +1,6 @@
-token_string = ""
+from os import environ
+
+token_string = environ.get("MONITORA_TOKEN")
+
+if not token_string or token_string == '':
+    raise Exception("Missing monitora token.")
