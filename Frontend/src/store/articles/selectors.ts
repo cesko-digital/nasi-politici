27dc73b20bd414a41d9ec7636b74197ca53b8c02
Michael Jenis <michael.jenis@socialbakers.com>
2020-05-10 16:08:33 +0200

    152-v-médiích-bug-náhledu

--- Frontend/src/store/articles/selectors.ts
+++ Frontend/src/store/articles/selectors.ts
@@ -8,9 +8,12 @@ export const getArticles = (store: AppState): ArticlesState['articles'] => store
 export const isLoading = (store: AppState): boolean => store.articles.loading
 
 export const getDetailNews = createSelector(getArticles, articles => {
-  return articles.map(a => ({
-    ...a,
-    source: a.source.replace(new RegExp('^www.'), ''),
-    published: dummyFormatDate(new Date(a.published)),
-  }))
+  return articles.map(article => {
+    const HTMLEntitiesStrippedPerex = article.perex.replace(/&#([0-9]{1,3});/gi, (match, numStr) => String.fromCharCode(parseInt(numStr, 10)))
+    return ({
+    ...article,
+    perex: HTMLEntitiesStrippedPerex,
+    source: article.source.replace(new RegExp('^www.'), ''),
+    published: dummyFormatDate(new Date(article.published)),
+  })})
 })
