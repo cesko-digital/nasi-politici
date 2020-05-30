03031d401f3482fdf036668592916e2ba0e65c5b
martinwenisch <55838596+martinwenisch@users.noreply.github.com>
2020-05-30 11:24:00 +0100

    Revert "Remove NAT gateway from AWS (cost savings) (#64)"
    This reverts commit 62a006065516d8e34e840d74a08c35c9050631b3.

--- infrastructure/aws/aws.tf
+++ infrastructure/aws/aws.tf
@@ -111,9 +111,18 @@ resource "aws_internet_gateway" "internet-gateway" {
   }
 }
 
+resource "aws_eip" "nat-gateway-ip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat-gateway" {
+  subnet_id = aws_subnet.public.id
+  allocation_id = aws_eip.nat-gateway-ip.id
+}
+
 # Routing table settings, we have 2 routing tables
 # 1. public -> have access directly to internet gateway
-# 2. private -> have access directly to internet gateway too (to save cost on NAT)
+# 2. private -> is routed to internet via NAT Gateway
 #
 # There is default table (set as main) without access to internet, only for local routes. Which is good default.
 resource "aws_route_table" "public-routes" {
@@ -134,7 +143,7 @@ resource "aws_route_table" "private-routes" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet-gateway.id
+    nat_gateway_id = aws_nat_gateway.nat-gateway.id
   }
 
   tags = {
