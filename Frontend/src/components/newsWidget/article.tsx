f9305620a7aa14bdf1ff29b7918a2a3149b8cae2
Boris Cirok <boris.cirok@tipsport.cz>
2021-04-13 10:05:13 +0200

    dezinfo weby
    přidání tagů, přidání dezinformačního tagu

--- Frontend/src/components/newsWidget/article.tsx
+++ Frontend/src/components/newsWidget/article.tsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import styles from './newsWidget.module.scss'
 
-import { Article } from '../../services/apiTypes'
+import { Article as ArticleType } from '../../services/apiTypes'
+import { ArticleTags } from './articleTags'
 
 type Props = {
-  article: Article
+  article: ArticleType
 }
 
 export function Article({ article }: Props) {
-  const { perex, title, published, source, url } = article
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  const { perex, title, published, source, url, is_fake_news } = article
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  const hasTags = !!is_fake_news
   return (
     <div className={styles.article}>
       <div className={styles.sources}>
         <div className={styles.source}>{source}</div>
         <div>, {published}</div>
       </div>
+      {hasTags && <ArticleTags article={article} />}
       <div>
         <a href={url} target="_blank" rel="noopener noreferrer" className={styles.headline}>
           {title}
---
+++ Frontend/src/components/newsWidget/articleTags.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import classNames from 'classnames'
+import styles from './newsWidget.module.scss'
+
+import { Article as ArticleType } from '../../services/apiTypes'
+
+type Props = {
+  article: ArticleType
+}
+
+export function ArticleTags({ article }: Props) {
+  // eslint-disable-next-line @typescript-eslint/camelcase
+  const { is_fake_news } = article
+  return (
+    <div className={styles.articleTags}>
+      {/* eslint-disable-next-line @typescript-eslint/camelcase */}
+      {is_fake_news && <div className={classNames(styles.articleTag, styles.articleTagRed)}>Dezinformační web</div>}
+    </div>
+  )
+}
