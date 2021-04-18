f3b59b79721593eeede809407b06c57fd11cc46b
Roman <rajchert.roman@gmail.com>
2021-04-18 12:11:09 +0200

    Add useGTM hook

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,8 @@
-import * as React from 'react'
+import React, { useEffect } from 'react'
 import Helmet from 'react-helmet'
 import { Route, Switch } from 'react-router-dom'
+import useGTM from '@elgorditosalsero/react-gtm-hook'
+
 import Footer from './components/footer/footer'
 import Header from './components/header/headerConnected'
 import AboutUs from './pages/about/about'
@@ -12,23 +14,29 @@ import ReportModal from './components/reportModal/reportModalConnected'
 import './App.scss'
 
 const App: React.FC = () => {
+  const { init, UseGTMHookProvider } = useGTM()
+
+  useEffect(() => init({ id: 'GTM-N5LZSMG' }), [])
+
   return (
-    <div className="App">
-      <Helmet>
-        <title>Naši Politici</title>
-      </Helmet>
-      <Header />
-      <div className="body">
-        <Switch>
-          <Route path="/" component={Homepage} exact />
-          <Route path="/detail/:id" component={Detail} />
-          <Route path="/o-projektu" component={AboutUs} />
-          <Route component={Error} />
-        </Switch>
+    <UseGTMHookProvider>
+      <div className="App">
+        <Helmet>
+          <title>Naši Politici</title>
+        </Helmet>
+        <Header />
+        <div className="body">
+          <Switch>
+            <Route path="/" component={Homepage} exact />
+            <Route path="/detail/:id" component={Detail} />
+            <Route path="/o-projektu" component={AboutUs} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+        <Footer />
+        <ReportModal />
       </div>
-      <Footer />
-      <ReportModal />
-    </div>
+    </UseGTMHookProvider>
   )
 }
 
