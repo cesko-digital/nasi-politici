70d323be2af6f902aad6071a07073fcb51a9c299
Roman Rajchert <rajchert.roman@gmail.com>
2021-07-27 11:10:43 +0200

    Fix not smae text at every noData widget (#134)

--- Frontend/src/components/emptyStates/noData/noData.tsx
+++ Frontend/src/components/emptyStates/noData/noData.tsx
@@ -2,16 +2,13 @@ import * as React from 'react'
 import { ReactComponent as NoDataImg } from 'assets/images/collection.svg'
 import styles from './noData.module.scss'
 
-const NoData: React.FC = () => {
+const NoData: React.FC<{ text?: string }> = ({ text }) => {
   return (
     <div className={styles.emptyState}>
       <div className={styles.circle}>
         <NoDataImg className={styles.image} />
       </div>
-      <div className={styles.title}>
-        Data nejsou dostupná z důvodu znepřístupnění registru Ministerstvem spravedlnosti na základě rozhodnutí
-        Ústavního soudu.
-      </div>
+      <div className={styles.title}>{text || <>Pro tuto kategorii zatím nemáme ověřená data :/</>}</div>
     </div>
   )
 }
