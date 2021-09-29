3231d78c8247d59e7ec7cce3e7a1d6ed242b37a8
Roman Rajchert <rajchert.roman@gmail.com>
2021-09-29 19:06:49 +0200

    Some edits

--- Frontend/src/components/filters/filters.tsx
+++ Frontend/src/components/filters/filters.tsx
@@ -1,17 +1,13 @@
 import React, { useCallback } from 'react'
 import { useRouteMatch } from 'react-router-dom'
-import classnames from 'classnames'
-import { ReactComponent as Search } from 'assets/images/search.svg'
 
 import styles from './filters.module.scss'
 
-export interface Props {}
-
-const Filters: React.FC<Props> = ({}) => {
-
+const Filters: React.FC<{}> = ({}) => {
   return (
     <div>
       <h3>Nebo vyberte z následujících atributů</h3>
+      <div className=""></div>
     </div>
   )
 }
