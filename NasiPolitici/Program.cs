d67bab5be439be5bfce6a7292ae1f2e1123f4cf7
martinwenisch <martin@muhu.digital>
2023-09-19 10:19:23 +0200

    Add axiom log drain

--- NasiPolitici/Program.cs
+++ NasiPolitici/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using Microsoft.Extensions.Configuration;
 
 namespace HlidacStatu.NasiPolitici
 {
@@ -8,6 +11,14 @@ namespace HlidacStatu.NasiPolitici
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
             var host = CreateHostBuilder(args).Build();
             host.Run();
         }
@@ -15,6 +26,7 @@ namespace HlidacStatu.NasiPolitici
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
